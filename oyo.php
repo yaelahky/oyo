67429da61b9364865c28d57629217960f1aaac01
ANANDA RIZKY Y <kiky.lenovo@gmail.com>
2019-11-13 00:18:11 +0700

    new api + resp

--- oyo.php
+++ oyo.php
@@ -412,7 +412,7 @@ $phone_number = trim(fgets(STDIN));
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'https://api.oyorooms.com/v2/users/generate_otp?phone='.$phone_number.'&nod=4&intent=login&sms_auto_retrieval=true&country_code=%2B62&version=20233&partner_app_version=20233&android_id='.generateRandomString(16).'&idfa=&sid=1551940465205');
+curl_setopt($ch, CURLOPT_URL, 'https://api.oyorooms.com/v2/users/generate_otp?phone='.$phone_number.'&nod=4&intent=login&sms_auto_retrieval=true&country_code=%2B1&version=20233&partner_app_version=20233&android_id='.generateRandomString(16).'&idfa=&sid=1551940465205');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 
@@ -479,14 +479,11 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
-    echo 'Error:' . curl_error($ch);
+    echo 'Error';
 }
 curl_close ($ch);
 
+echo $result;
+
 $json = json_decode($result);
-
-if($json->success == 0){
-	echo "Berhasil mendaftar OYO";
-}
-
-
+echo $json;
